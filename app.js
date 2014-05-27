1069e684f910e84507b12b833a1fefc4baf7fdc4
redky <redky@qq.com>
2014-05-27 18:10:20 +0800

    add decode function

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ var escaper = /\\|'|"|\r|\n|\u2028|\u2029/g;
 $( '#head' ).on( 'click', 'button', function(e) {
   var id = this.id;
   var string = code.val().replace( escaper, '\\$&' );
-  eval( 'var ret = ' + id + '("' + string + '")' );
+  var setting = {};
+  if ( id === 'decode' ) {
+    setting = {
+      base: this.dataset.base,
+      left: this.dataset.left,
+      right: this.dataset.right
+    };
+    var ret = decode( string, setting );
+  }
+  else {    
+    eval( 'var ret = ' + id + '("' + string + '")' );    
+  }
   result.val( ret );
   return false;
 });
 
+function change() {
+  var t = code.val();
+  code.val( result.val() );
+  result.val( t );
+}
 
+$( document ).on( 'keydown', function(e) {
+  if ( e.altKey && e.which === 67 ) {
+    change();
+  }
+});
